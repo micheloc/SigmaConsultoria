a9f3def09334f2e19e73a64586bbc126bee547da
Michel Oliveira <michel.o.c@hotmail.com>
2024-11-27 14:39:20 -0300

    Criação da pasta Assets.

--- src/routes/components/stack.login.tsx
+++ src/routes/components/stack.login.tsx
@@ -1,4 +1,4 @@
-// import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 // import { createStackNavigator } from '@react-navigation/stack';
 
 // import Login from 'screens/Navegacao/navLogin';
