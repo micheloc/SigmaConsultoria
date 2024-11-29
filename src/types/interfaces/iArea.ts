c8582f2097c719ed0d0417062b298abd451ab5f9
Michel Oliveira <michel.o.c@hotmail.com>
2024-11-29 14:25:36 -0300

    atualização da tela de cadastro do cliente.

---
+++ src/types/interfaces/iArea.ts
@@ -0,0 +1,10 @@
+interface iArea {
+  objID: string;
+  idFazenda: string;
+  nome: string;
+  hectares: number;
+  created: Date | null | undefined;
+  updated: Date | null | undefined;
+}
+
+export default iArea;
